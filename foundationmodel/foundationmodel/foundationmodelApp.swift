c453aa8681e0b004701ed7d17e94a2194b53627a
swiftcrew10 <swiftcrew10@gmail.com>
2025-07-03 14:45:11 -0600

    Initial commit: Foundation Model Swift project
    - Added README.md with project description
    - Added Package.swift for Swift Package Manager
    - Added main.swift with Foundation Model implementation
    - Added comprehensive .gitignore for Swift projects

---
+++ foundationmodel/foundationmodel/foundationmodelApp.swift
@@ -0,0 +1,17 @@
+//
+//  foundationmodelApp.swift
+//  foundationmodel
+//
+//  Created by Joel Vargas on 03/07/25.
+//
+
+import SwiftUI
+
+@main
+struct foundationmodelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ foundationmodel/foundationmodelTests/foundationmodelTests.swift
@@ -0,0 +1,17 @@
+//
+//  foundationmodelTests.swift
+//  foundationmodelTests
+//
+//  Created by Joel Vargas on 03/07/25.
+//
+
+import Testing
+@testable import foundationmodel
+
+struct foundationmodelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
